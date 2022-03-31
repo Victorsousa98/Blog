8ce22e4150697c9e4cce00608c9734d2ca7069e2
Victorsousa98 <victorsousa708@gmail.com>
2022-03-31 20:55:27 -0300

    adiciona crud de categorias

--- routes/admin.js
+++ routes/admin.js
@@ -53,8 +53,9 @@ router.post("/categorias/nova", (req, res) => {
     }
 });
 
+//editar categorias
 router.get("/categorias/edit/:id", (req, res) => {
-    Categoria.findOne({_id: req.params.id}).lean().then((categoria) => {
+    Categoria.findOne({_id: req.params.id}).lean().then((categoria) => {//lean() retorna um objeto json, sem o mongoose
         res.render("admin/editcategorias", {categoria: categoria});
     }).catch((err) => {
         req.flash("error_msg", "Esta categoria não existe");
@@ -62,7 +63,33 @@ router.get("/categorias/edit/:id", (req, res) => {
     });
 });
 
+router.post("/categorias/edit", (req, res) => {
+    Categoria.findOne({_id: req.body.id}).then((categoria) => {
+        categoria.nome = req.body.nome;
+        categoria.slug = req.body.slug;
 
+        categoria.save().then(() => {
+            req.flash("success_msg", "Categoria editada com sucesso!");
+            res.redirect("/admin/categorias");
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno ao salvar a edição da categoria");
+            res.redirect("/admin/categorias");
+        });
 
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno");
+        res.redirect("/admin/categorias");
+    });
+});
+
+router.post("/categorias/deletar", (req, res) => {
+    Categoria.deleteOne({_id: req.body.id}).then(() => {
+        req.flash("success_msg", "Categoria deletada com sucesso!");
+        res.redirect("/admin/categorias");
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao deletar a categoria");
+        res.redirect("/admin/categorias");
+    });
+});
 
 module.exports = router
